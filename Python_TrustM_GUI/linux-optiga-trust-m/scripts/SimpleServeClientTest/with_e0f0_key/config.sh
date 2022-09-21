303c68a14d8c3bd5e0c8550233b06a7b9c841894
Ye Ying (IFAP CSS SMD AP TM SAE ITS) <Ying.Ye@infineon.com>
2022-09-21 11:08:14 +0800

    1)Deleted linux-optiga-trust-m folder

--- Python_TrustM_GUI/linux-optiga-trust-m/scripts/SimpleServeClientTest/with_e0f0_key/config.sh
+++
@@ -1,2 +0,0 @@
-EXEPATH="/home/pi/linux-optiga-trust-m/bin"
-CERT_PATH="/home/pi/linux-optiga-trust-m/scripts/certificates"
--- Python_TrustM_GUI/linux-optiga-trust-m/scripts/misc/config.sh
+++
@@ -1,3 +0,0 @@
-PROJECT_DIR="/home/pi/Desktop/WIP1/linux-optiga-trust-m"
-EXEPATH="$PROJECT_DIR/bin"
-CERT_PATH="$PROJECT_DIR/scripts/certificates"
