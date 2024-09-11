14ab98d580032c37f6eb10a64a9a6fdfacadf295
ying-css <ying.ye@infineon.com>
2024-09-11 10:29:24 +0800

    1)cleaned the code

--- Python_TrustM_GUI/misc_dialogs.py
+++ Python_TrustM_GUI/misc_dialogs.py
@@ -1,10 +1,8 @@
 import wx
 import shell_util as exec_cmd
 import tab4_protected as t4
-import tab6_cloud as t6
 
 
-# Generic File Editor for AWS Cloud connectivity (for tab6 use)
 class EditorFrame(wx.Dialog):
     def __init__(self, parent, title, filename):
         wx.Dialog.__init__(self, parent, wx.ID_ANY, title=title)
@@ -42,19 +40,3 @@ class EditorFrame(wx.Dialog):
         user_input.Destroy()
         self.Destroy()
 
-
-# Warning Dialog for AWS Region mis-match
-# Note by default the AWS C application is set to us-east-1.
-class AwsRegionWarning(wx.MessageDialog):
-    def __init__(self, parent, title):
-        wx.MessageDialog.__init__(self, parent, message="", caption=title, style=wx.OK | wx.ICON_WARNING)
-
-        self.SetMessage("WARNING, as you have set the AWS region to a NON us-west-2 region, when attempting to publish to the AWS Cloud, the application will crash.\n\n\
-To fix this problem:\n\
-    Edit the file, 'aws_iot_config.h' in /aws_trustm/sample_apps/eHealthTrustM/eHealthDevice, and edit the value AWS_IOT_MQTT_HOST with the correct endpoint.\n\
-    Or, just use the AWS region us-west-2.")
-        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
-
-    def OnCloseWindow(self, evt):
-        self.EndModal(-1)
-        self.Destroy()
